549db0885c91f395f73167e5a1dd549bd81e8393
David <75371070+whiteandwild@users.noreply.github.com>
2026-05-12 19:53:27 +0200

    przycisk template

---
+++ Button.py
@@ -0,0 +1,88 @@
+from abc import ABC , abstractmethod
+from enum import Enum, auto
+from dataclasses import dataclass
+
+class ButtonState(Enum):
+    NORMAL = auto()
+    HOVER = auto()
+    PRESSED = auto()
+    DISABLED = auto()
+    LOADING = auto()
+
+class Shapes(Enum):
+    RECTANGLE = auto()
+    ROUNDED = auto()
+    CIRCLE = auto()
+
+@dataclass(frozen=True)
+class ButtonStyle():
+    bg_color: str
+    text_color: str
+
+    hover_bg_color: str
+    hover_text_color: str
+
+    border_radius: int
+    border_color: str
+    border_width: int
+
+    shape: Shapes
+
+
+class ButtonBackend(ABC):
+
+    @abstractmethod
+    def create(self, label: str , onclick: callable , style: ButtonStyle):
+        # Tworzenie przycisku
+        pass
+
+    @abstractmethod
+    def set_label(self , label: str ):
+        # zmiana podpisu
+        pass
+
+    @abstractmethod
+    def apply_state(self, state: ButtonState, style: ButtonStyle):
+        # zmiana stanu
+        pass
+
+    @abstractmethod
+    def update_style(self, style: ButtonStyle):
+        pass
+
+class Button:
+
+    def __init__(self , label: str, backend: ButtonBackend , style: ButtonStyle, action:callable = None ):
+        self.label = label
+        self.style = style
+        self._backend = backend
+        self.action = action
+
+        self._state = ButtonState.NORMAL
+
+    def render(self):
+        widget = self._backend.create(label=self.label , onclick=self.action , style=self.style)
+        return widget
+    
+    def update_label(self, new_text: str):
+        self.label = new_text
+        self._backend.set_label(new_text)
+
+    def update_style(self, new_style: ButtonStyle):
+        self.style = new_style
+        self._backend.update_style(self.style)
+
+    def set_state(self, new_state:ButtonState):
+        if self._state == new_state:
+            return
+        self._state = new_state
+        self._backend.apply_state(self._state, self.style)
+
+    def _handle_click(self):
+
+        if self._status == ButtonState.NORMAL:
+            if self.action:
+                self.action()
+        else:
+            pass
+  
