286dfc3ad637cd774d1de717582d66326f4d6b4a
David <75371070+whiteandwild@users.noreply.github.com>
2026-05-28 21:07:34 +0200

    dalsza czesc napraw

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ from kivy.uix.screenmanager import ScreenManager
 
 import Style.stylesMain as styles
 
-from screenHandler import ScreenHandler
+from screenHandler import *
 
 class StudentPlannerApp(App):
 
 
     def build(self):
-
         with open('translation.json' , 'r' , encoding='utf-8') as file:
             data = json.load(file)
         
@@ -32,7 +31,7 @@ class StudentPlannerApp(App):
 
         #  screen_mojePrzedmioty = MojePrzedmiotyScreen(name='moje_przedmioty_screen')
 
-        self.change_screen('mySubjects')
+        self.change_screen(target_screen='mySubjects')
         return root_widget
         # Builder.load_file("Style/styles.kv")
         # Builder.load_file("kv/topbar.kv")
@@ -45,11 +44,35 @@ class StudentPlannerApp(App):
         # screen_manager.add_widget(StartKalendarz(name="start"))
 
 
-    def translate(self, key):
-        return ScreenHandler.translate(key)
-    
+    def translate(self, text:str):
+                return self.translations[self.language].get(text)
+
     def change_screen(self, target_screen , **kwargs):
-        ScreenHandler.change_screen(target_screen , **kwargs)
+        if not self.sm.has_screen(target_screen):
+            
+            screens = {
+                'mySubjects': {'class': MojePrzedmiotyScreen, 'kv': 'kv/mojePrzedmioty.kv'},
+                'subjectDetails': {'class': SzczegolyPrzedmiotuScreen, 'kv': 'kv/szczegolyPrzedmiotu.kv'},
+                'calendar' : {'class':StartKalendarz, 'kv': 'kv/calendar.kv'}
+            }
+            
+            config = screens.get(target_screen)
+            
+            if config:
+                
+                Builder.load_file(config['kv'])
+                new_screen = config['class'](name=target_screen)
+                self.sm.add_widget(new_screen)
+            else:
+                print(f"Error invaild scren name {target_screen}")
+                return
+            
+        new_screen = self.sm.get_screen(target_screen)
+
+        for klucz, wartosc in kwargs.items():
+            setattr(new_screen, klucz, wartosc)
+
+        self.sm.current = target_screen
 
     def change_language(self):
         if self.language == "pl":
@@ -58,3 +81,11 @@ class StudentPlannerApp(App):
             self.language = "pl"
 
         print("Zmieniono język na:", self.language)
+    
+    def get_subjects_from_db(self):
+    
+        return [
+            SubjectData(title="Algebra", teacher="Jan Kowalski", status="completed" , note=""),
+            SubjectData(title="ASD", teacher="JŚW", status="completed", note=""),
+            SubjectData(title="Bazy Danych", teacher="Anna Nowak", status="completed", note="")
+        ]
--- screenHandler.py
+++ screenHandler.py
@@ -1,38 +1,3 @@
-from kivy.lang import Builder
-
-from views.mojePrzedmioty import MojePrzedmiotyScreen , SzczegolyPrzedmiotuScreen
+from views.mojePrzedmioty import MojePrzedmiotyScreen , SzczegolyPrzedmiotuScreen , SubjectData
 from views.startKalendarz import StartKalendarz
 
-
-
-class ScreenHandler():
-        
-        def translate(self, text:str):
-                return self.translations[self.language].get(text)
-
-        def change_screen(self, target_screen , **kwargs):
-            if not self.sm.has_screen(target_screen):
-                
-                screens = {
-                    'mySubjects': {'class': MojePrzedmiotyScreen, 'kv': 'kv/mojePrzedmioty.kv'},
-                    'subjectDetails': {'class': SzczegolyPrzedmiotuScreen, 'kv': 'kv/szczegolyPrzedmiotu.kv'},
-                    'calendar' : {'class':StartKalendarz, 'kv': 'kv/calendar.kv'}
-                }
-                
-                config = screens.get(target_screen)
-                
-                if config:
-                    
-                    Builder.load_file(config['kv'])
-                    new_screen = config['class'](name=target_screen)
-                    self.sm.add_widget(new_screen)
-                else:
-                    print(f"Error invaild scren name {target_screen}")
-                    return
-                
-            new_screen = self.sm.get_screen(target_screen)
-
-            for klucz, wartosc in kwargs.items():
-                setattr(new_screen, klucz, wartosc)
-
-            self.sm.current = target_screen
